323a3c6c7beafdda0e83380feefbdec286823d03
futuremaker <futuremaker019@gmail.com>
2025-02-26 23:51:42 +0900

    [ fix ] 메인페이지/임시적으로 모의고사 페이지 이동 추가

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Main = () => {
     return (
-        <div className={'text-9xl text-blue-500'}>
-            main
-        </div>
+        <>
+            <div className={'text-9xl text-blue-500'}>
+                main
+            </div>
+            <Link to={'/exams'}>
+                모의고사
+            </Link>
+        </>
+
     );
 };
 
