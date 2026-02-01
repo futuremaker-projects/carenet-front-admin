af2ad7821353e2fc19ce9a7f0d395699b45aa1dd
futuremaker <futuremaker019@gmail.com>
2026-02-01 12:54:40 +0900

    [feat] CBT/cbt에서 보여줄 모의고사 목록 화면 추가

---
+++ src/pages/exam/cbt/CbtExams.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+/**
+    `cbt`에서 보여줄 모의고사 목록
+        - 사용자가 구매한 모의고사 목록이 조회되도록 개발필요
+ */
+const CbtExams = () => {
+
+    // 처음 풀어보는 모의고사는 모의고사만 나오고 문제를 풀었다면 채점 결과도 같이 나와야 함
+
+    return (
+        <div>
+
+        </div>
+    );
+};
+
+export default CbtExams;
